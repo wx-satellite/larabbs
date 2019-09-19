819e1d8a223e3f5c1614eb37396c4ee87eb8ae78
wxsatellite <1453085314@qq.com>
2019-09-19 13:26:41 +0000

    生产数据自动填充补充时间戳更新

--- database/migrations/2019_09_18_220659_seed_categories_data.php
+++ database/migrations/2019_09_18_220659_seed_categories_data.php
@@ -20,18 +20,26 @@ class SeedCategoriesData extends Migration
             [
                 'name'        => '分享',
                 'description' => '分享创造，分享发现',
+                "created_at" => date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s")
             ],
             [
                 'name'        => '教程',
                 'description' => '开发技巧、推荐扩展包等',
+                "created_at" => date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s")
             ],
             [
                 'name'        => '问答',
                 'description' => '请保持友善，互帮互助',
+                "created_at" => date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s")
             ],
             [
                 'name'        => '公告',
                 'description' => '站点公告',
+                "created_at" => date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s")
             ],
         ];
 
